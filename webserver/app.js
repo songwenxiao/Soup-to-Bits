b1a9922edb0b3a86d227dbaaf1a56de5ceeab6e8
Wenxiao Song <song.we@husky.neu.edu>
2016-05-28 14:22:10 -0400

    webserve

---
+++ webserver/app.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var subSocket = require('./lib/subscribe');
+var badges = require('./models/badges');
+
+server.listen(3001, function(){
+        console.log('Server is running on port %d', 3001);
+});
+
+
+/**
+ *  Server static assets out of public
+ */
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res){
+	res.sendfile('./public/index.html');
+});
+
+io.sockets.on('connection', function(socket){
+	badges.get(function(err,data){
+		if (err) return;
+		data.forEach(function(badge){
+			socket.emit('badge', badge);
+		});
+	});
+});
+
+subSocket.on('message', function(message){
+	io.sockets.emit('badge',message);
+});
+
+
+
---
+++ webserver/public/js/souptobits.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$(function(){
+
+	var socket = io.connect();
+	
+	socket.on('badge',function(badge){
+		var $img = $('<img src="'+ badge.badge_id + '"alt="Code School Badge">');
+		$('body').prepend($img);
+	});
+});
+
+
